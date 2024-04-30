ad7417def3af3e3dcec625ca54480f9a1c6bbac7
Maicon Fernandes <86450189+Maicon-Fernandes@users.noreply.github.com>
2024-04-29 22:43:50 -0300

    ,

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,14 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        byte Idade = 26;
+        short ano = 2024;
+        int CEP = 93546060;
+        long CPF = 11111111111L;
+        float PI = 3.14f;
+        double meuSalario = 2830.80;
+
+        final double VALOR_DE_PI = 3.14; //no java o Final serve para fixar o valor da variavel.
+
     }
 }
